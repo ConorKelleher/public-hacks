0eb8bb689bca0510b36ce220e3b99f6afa40bf93
Conor Kelleher <conor.kelleher@stitcherads.com>
2019-12-21 15:56:25 +0000

    Setting up cow game loop

--- Unreal Projects/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ Unreal Projects/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,29 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    PrintLine(TEXT("Welcome to Bulls & Cows!"));
+
+    HiddenWord = TEXT("bother");
+    Lives = 5;
+
+    PrintLine(TEXT("Guess the 4 letter word"));
+    PrintLine(TEXT("Hit <enter> after your guess to submit..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
 
+    if (Input == HiddenWord) {
+        PrintLine(TEXT("You got it!"));
+    } else {
+        PrintLine(TEXT("NOOP! You are a losing person"));
+
+        if (--Lives > 0) {
+            PrintLine(TEXT("Try again. You have some lives left"));
+        } else {
+            PrintLine(TEXT("You're out of lives. Tough luck!"));
+        }
+    }
 }
